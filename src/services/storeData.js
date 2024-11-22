3b9040459ee04b55054f116cdbe9c78b41fcd870
SarhanPratama <pratamasarhan@gmail.com>
2024-11-22 20:30:35 +0700

    ganti port

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,8 +1,9 @@
 const { Firestore } = require('@google-cloud/firestore');
-const db = new Firestore();
  
 async function storeData(id, data) {
- 
+  // Sesuaikan databaseId dengan instances yang dibuat di GCP
+  const db = new Firestore({databaseId: "serta-mulia"}); 
+
   const predictCollection = db.collection('prediction');
   return predictCollection.doc(id).set(data);
 }
