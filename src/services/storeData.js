997941cc73bca3483c2b4bae6ac683dbda4706b0
SarhanPratama <pratamasarhan@gmail.com>
2024-11-22 18:55:45 +0700

    first

---
+++ src/services/storeData.js
@@ -0,0 +1,10 @@
+const { Firestore } = require('@google-cloud/firestore');
+const db = new Firestore();
+ 
+async function storeData(id, data) {
+ 
+  const predictCollection = db.collection('prediction');
+  return predictCollection.doc(id).set(data);
+}
+ 
+module.exports = storeData;
